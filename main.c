bd638e87851e38518bdc99a1b2a0694d7f863d7a
EthanDever <159584445+EthanDever@users.noreply.github.com>
2024-02-27 17:45:38 -0400

    Added RX initialization and global variables

--- main.c
+++ main.c
@@ -1,92 +1,116 @@
-/*
- * File:   main.c
- * Author: mchiass3
- *
- * Created on February 8, 2024, 6:54 PM
- */
-
+#pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (EC above 8MHz; PFM set to high power)
+#pragma config RSTOSC = HFINT32
+#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON
 
 #include <xc.h>
-#pragma config RSTOSC = HFINT32
+
 #pragma config WDTE = OFF
 
+#define _XTAL_FREQ 32000000
 
 /*
 a global variable in place of a parameter to be used by the ISR
 */
-int byteToSend;
+int byteToSend = 1;
+int DataIn[8];
+int Index = 0;
 
 
+void waitForIt(){
+    
+    while(1){
+        if(TX1STAbits.TRMT == 1)
+            break;
+    }
+
+//boom
+}
 
 void setUp(void){
+    
+    RC5PPS = 0x10; //PPS OUTPUT SIGNAL ROUTING OPTIONS: 0x10 = TX/CK
+    RXPPS = 0x16; // PPS INPUT SIGNAL ROUTING: 0x16 = RC6
+    
+    TRISCbits.TRISC6 = 1;
+    TRISCbits.TRISC5 = 1;
+    TX1STAbits.TXEN = 1; //Transmit Enable bit: 1 = Transmit enabled
+    TX1STAbits.SYNC = 0; //EUSART Mode Select bit: 0 = Asynchronous mode
+    
+    RC1STAbits.SPEN = 1; // Serial Port Enable bit: 1 = Serial port enabled
+    RC1STAbits.CREN = 1;
+    
+    BAUD1CONbits.BRG16 = 1; //16-bit Baud Rate Generator bit: 1 = 16-bit Baud Rate Generator is used
+                            //0 = 8-bit Baud Rate Generator is used
+    
+    TX1STAbits.BRGH = 1; //High Baud Rate Select bit: Asynchronous mode: 1= High speed
+    SP1BRGL = 0x44; //Lower eight bits of the Baud Rate Generator
+    SP1BRGH = 0x00;       //Upper eight bits of the Baud Rate Generator
+    //baud rate = 115200
+    
+    INTCONbits.GIE = 1; //Global Interrupt Enable bit: 1 = Enables all active interrupts
+    INTCONbits.PEIE = 1; //Peripheral Interrupt Enable bit: 1 = Enables all active peripheral interrupts
+    PIE3bits.RCIE = 1;   // Receive interrupt enabled
+    ANSELCbits.ANSC6 = 0; // RC6 is digital input
+    //PIE3bits.TXIE = 1; // USART Transmit Interrupt Enable bit: 1 = Enables the USART transmit interrupt
+    
+}
+
+void __interrupt() _ISR(){  
+    
+    if(PIR3bits.TXIF == 1){ //transmit flag
+        PIE3bits.TXIE = 0;
+        waitForIt();//boom
+    }
+    
+    
+   if(PIR3bits.RCIF == 1){ //receive flag
        
-    TX1STAbits.TXEN = 1;
-    TX1STAbits.SYNC = 0;
+        DataIn = RC1REG;
+//       DataIn[Index] = RC1REG;
+//       Index = Index + 1;
+//       if (Index >= 7)
+//       {
+//           Index = 7;
+//       }
+       
+    }
+        
     
-    RC1STAbits.SPEN = 1;
-    
-    BAUD1CONbits.BRG16 = 1;
-    
-    TX1STAbits.BRGH = 1;
-    
-    SP1BRGL = 0b01000000;
-    SP1BRGH = 0b11;
-    
-    INTCONbits.GIE = 1;
-    INTCONbits.PEIE = 1;
-    PIE3bits.TXIE = 1;
-    
-    RC5PPS = 0x10;
-      
 }
 
-
-void __interrupt() _transmitByte(){
-
-    TX1REGbits.TX1REG = byteToSend; 
-   
-}
 
 void transmitByte(int byteToSend_IN){
     
-    byteToSend = byteToSend_IN;
-    _transmitByte(); //ISR
-    
-}
-
-
-void setByte(int byte_IN){ //not currently in use but would be helpful to implement for cleaner code
-
-    byteToSend = byte_IN;
-
+    byteToSend = byteToSend_IN; //set global var
+    TX1REGbits.TX1REG = byteToSend; //set the register to hold the value
+    PIE3bits.TXIE = 1; // USART Transmit Interrupt Enable bit: 1 = Enables the USART transmit interrupt
+  
 }
  
 
+void transmitSync(){
+    
+    transmitByte(0xFE); // sync
+
+    transmitByte(0x19); // sync
+
+}
+
 void transmitCommonMotor(void){
 
-    //0xFE1901060400;
-//    byteToSend = 0xFE;
-//    _transmitByte();
-    transmitByte(0xFE);
+    //0xFE 19 (sink) 
+    // 01060400;
     
-//    byteToSend = 0x19;
-//    _transmitByte();
-    transmitByte(0x19);
+    //sink has already been called
     
-//    byteToSend = 0x01;
-//    _transmitByte();
     transmitByte(0x01);
-    
-//    byteToSend = 0x06;
-//    _transmitByte();
+
     transmitByte(0x06);
-    
-//    byteToSend = 0x04;
-//    _transmitByte();
+
     transmitByte(0x04);
     
-//    byteToSend = 0x00;
-//    _transmitByte();
     transmitByte(0x00);
 
 }
@@ -94,28 +118,42 @@ void transmitCommonMotor(void){
 
 void testPulseMotor(int motorA_dir_IN, int motorA_speed_IN, int motorB_dir_IN, int motorB_speed_IN){
     
-    //TX1STAbits.TXEN = 1;
+    transmitSink();
     
     transmitCommonMotor();
     
-    //byteToSend = motorA_dir_IN;
-    //_transmitByte();
     transmitByte(motorA_dir_IN);
     
-    //byteToSend = motorA_speed_IN;
-    //_transmitByte();
     transmitByte(motorA_speed_IN);
+        
+    transmitByte(motorB_dir_IN);  
     
-    
-    //byteToSend = motorB_dir_IN;
-    //_transmitByte();
-    transmitByte(motorB_dir_IN);
-    
-    //byteToSend = motorB_speed_IN;
-    //_transmitByte();
     transmitByte(motorB_speed_IN);
 
 }
+
+void getPCLS(){
+
+    //recive register has a buffer of 2 bytes
+    
+    //ask pcls to get data from controller
+    
+    transmitSync();
+    
+    transmitByte(0x01); // MSG ID
+    
+    transmitByte(0x04); // MSG ID
+    
+    transmitByte(0x00);
+    
+    transmitByte(0x00);
+    
+    //
+    
+
+}
+
+
 
 void main(void) {
     
@@ -126,15 +164,25 @@ void main(void) {
     //0x64 is max speed
     
     while(1){
+        waitForIt();//boom
         
-        testPulseMotor(0x01, 0x64, 0x00, 0xFF);
         
+        //testPulseMotor(0x01, 0x64, 0x00, 0x00);
+        getPCLS();
+        
+      
+        //transmitByte(0x56);
+
+        //transmitByte(0xF1);
+        
+        //transmitByte(0x00);
+        
+        
+        __delay_ms(250);
     } 
     
     return;
 }
-
-
 
 
 
