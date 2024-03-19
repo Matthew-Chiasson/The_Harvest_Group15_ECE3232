fcdda74ecfa84cb76d34009b0ad032fcff71e9cd
tstandenCS1003 <90403161+tstandenCS1003@users.noreply.github.com>
2024-03-19 15:48:20 -0300

    Add files via upload
    Trevor's attempt to read data into global variables

---
+++ data_sorting.c
@@ -0,0 +1,195 @@
+#include "data_sorting.h"
+
+uint8_t messages[100];
+uint8_t *messages_origin = messages;
+uint8_t *read_here = messages;
+uint8_t *receive_here = messages;
+uint8_t *end_of_messages = messages+100;
+uint8_t data_sorting_error = 0;
+
+uint8_t teamID;
+uint8_t playerID;
+uint8_t overallHealthLSB;
+uint8_t overallHealthMSB;
+uint8_t shieldCodeFlag;
+uint8_t repairCodeFlag;
+
+uint8_t rightJoyStickXLSB;
+uint8_t rightJoyStickXMSB;
+uint8_t rightJoyStickYLSB;
+uint8_t rightJoyStickYMSB;
+uint8_t leftJoyStickYLSB;
+uint8_t leftJoyStickYMSB;
+uint8_t leftJoyStickXLSB;
+uint8_t leftJoyStickXMSB;
+uint8_t switchALSB;
+uint8_t switchAMSB;
+uint8_t switchBLSB;
+uint8_t switchBMSB;
+uint8_t switchCLSB;
+uint8_t switchCMSB;
+uint8_t switchDLSB;
+uint8_t switchDMSB;
+uint8_t VRALSB;
+uint8_t VRAMSB;
+uint8_t VRBLSB;
+uint8_t VRBMSB;
+
+int check()
+{
+    if(read_here == receive_here)
+        return 1;
+    return 0;
+}
+
+void end(){
+    if(read_here == end_of_messages)
+        read_here = messages_origin;
+}
+
+void get_pcls_info()
+{
+    uint16_t payload;
+    uint8_t searching_error = 110;
+    uint8_t search_count = 0;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    payload = *read_here;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    
+    payload = payload << 8;
+    payload = payload || *read_here;
+    
+    while(1)
+    {
+        if ((receive_here > read_here) & (receive_here-read_here > 21))
+            break;
+        else if((read_here > receive_here)& (read_here-receive_here < 79))
+            break;
+            
+        search_count ++;
+        if(search_count >= searching_error)
+        {
+            data_sorting_error = 1;
+            return;
+        }
+    }
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    teamID = *read_here;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    playerID = *read_here;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    overallHealthLSB = *read_here;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    overallHealthMSB = *read_here;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    shieldCodeFlag = *read_here;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    repairCodeFlag = *read_here;
+}
+
+
+void get_user_data()
+{
+    uint16_t payload = *read_here;
+    uint8_t searching_error = 110;
+    uint8_t search_count = 0;
+    
+    read_here ++;
+    end();
+    if(check())
+        return;
+    payload = payload << 8;
+    payload = payload || *read_here;
+    
+    while(1)
+    {
+        if ((receive_here > read_here) & (receive_here-read_here > 7))
+            break;
+        else if((read_here > receive_here) & (read_here-receive_here < 93))
+            break;
+            
+        search_count ++;
+        if(search_count >= searching_error)
+        {
+            data_sorting_error = 1;
+            return;
+        }
+    }
+    
+}
+
+void sort_data()
+{
+    uint8_t searching_error = 110;
+    uint8_t search_count = 0;
+
+    while(*read_here != 0xFE)
+    {
+        if(check())
+            return;
+        read_here ++;
+        end();
+        search_count ++;
+        if(search_count >= searching_error)
+        {
+            data_sorting_error = 1;
+            return;
+        }
+    }
+    
+    read_here ++;           // sync bit #2
+    end();
+    if(check())
+        return;
+    if(*read_here != 0x19)
+        return;
+    
+    read_here ++;           // message LSB
+    end();
+    if(check())
+        return;
+    
+    read_here ++;           // message MSB
+    end();
+    if(check())
+        return;
+
+    if (*read_here == 0x04)
+        get_pcls_info();
+    //else if(*read_here == 0x05)
+      //  get_user_data();
+    
+    return;    
+}
---
+++ newmain1.c
@@ -0,0 +1,217 @@
+#pragma config FEXTOSC = OFF    // External Oscillator mode selection bits (EC above 8MHz; PFM set to high power)
+#pragma config RSTOSC = HFINT32
+#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON
+
+#include <xc.h>
+#include <stdbool.h>
+#include "motorControl.h"
+#include "data_sorting.h"
+
+#pragma config WDTE = OFF
+
+#define _XTAL_FREQ 32000000
+
+
+/*
+a global variable in place of a parameter to be used by the ISR
+*/
+int byteToSend = 1;
+//int dataIn[208]; // in data_sorting.h now called messages[100]
+int payloadSize = 0;
+int index = 0;
+bool ReceiveComplete;
+int here = 0;
+
+void waitForIt(){
+    
+    while(1){
+        if(TX1STAbits.TRMT == 1)
+            break;
+    }
+
+//boom
+}
+
+void setUp(void){
+    
+    RC5PPS = 0x10; //PPS OUTPUT SIGNAL ROUTING OPTIONS: 0x10 = TX/CK
+    RXPPS = 0x16; // PPS INPUT SIGNAL ROUTING: 0x16 = RC6
+    
+    TRISCbits.TRISC6 = 1;
+    TRISCbits.TRISC5 = 1;
+    TX1STAbits.TXEN = 1; //Transmit Enable bit: 1 = Transmit enabled
+    TX1STAbits.SYNC = 0; //EUSART Mode Select bit: 0 = Asynchronous mode
+    
+    RC1STAbits.SPEN = 1; // Serial Port Enable bit: 1 = Serial port enabled
+    RC1STAbits.CREN = 1;
+    
+    BAUD1CONbits.BRG16 = 1; //16-bit Baud Rate Generator bit: 1 = 16-bit Baud Rate Generator is used
+                            //0 = 8-bit Baud Rate Generator is used
+    
+    TX1STAbits.BRGH = 1; //High Baud Rate Select bit: Asynchronous mode: 1= High speed
+    SP1BRGL = 0x44; //Lower eight bits of the Baud Rate Generator
+    SP1BRGH = 0x00;       //Upper eight bits of the Baud Rate Generator
+    //baud rate = 115200
+    
+    INTCONbits.GIE = 1; //Global Interrupt Enable bit: 1 = Enables all active interrupts
+    INTCONbits.PEIE = 1; //Peripheral Interrupt Enable bit: 1 = Enables all active peripheral interrupts
+    PIE3bits.RCIE = 1;   // Receive interrupt enabled
+    ANSELCbits.ANSC6 = 0; // RC6 is digital input
+}
+
+void identifyMSG(int msg_In[]){
+
+    //message bytes
+    int msg_ID_byte1 = msg_In[2];
+    int msg_ID_byte2 = msg_In[3];
+
+    //do stuff based on message bytes
+    if(msg_ID_byte1 == 2 && msg_ID_byte2 == 4){ //PCLS
+    
+        //do code for PCLS
+        here = 1; //for testing purposes
+        
+    
+    }//else if other stuff (not yet implemented)
+
+}
+
+
+void __interrupt() _ISR(){  
+    
+    if(PIR3bits.TXIF == 1){ //transmit flag
+        PIE3bits.TXIE = 0;
+        waitForIt();//boom
+    }
+
+    
+   if(PIR3bits.RCIF == 1){ //receive flag
+       
+        if(index < 6 ){
+            *receive_here = RC1REG;
+            receive_here ++;
+            
+            end();
+            messages[1] = RC1REG;
+            //dataIn[index] = RC1REG;
+        }
+        else if(index == 6){
+            payloadSize = *(receive_here - 1) + *(receive_here - 2) +6;
+            //payloadSize = dataIn[4] + dataIn[5] + 6;
+            //dataIn[index] = RC1REG;
+            *receive_here = RC1REG;
+            receive_here ++;
+            end();
+        }
+        else if(index < payloadSize){
+            //dataIn[index] = RC1REG;
+            *receive_here = RC1REG;
+            receive_here ++;
+            end();
+        }
+        index += 1;
+       
+        /*if(index == payloadSize){
+            index = 0;
+            
+            int *message = (int *)malloc(payloadSize * sizeof(int));
+            
+            for(int j = 0; j < (payloadSize); j++){       
+                message[j] = dataIn[j];
+            }
+            
+            identifyMSG(message); 
+            free(message);  
+             
+        }*/
+    }
+}
+        
+
+void transmitByte(int byteToSend_IN){
+    
+    byteToSend = byteToSend_IN; //set global var
+    TX1REGbits.TX1REG = byteToSend; //set the register to hold the value
+    PIE3bits.TXIE = 1; // USART Transmit Interrupt Enable bit: 1 = Enables the USART transmit interrupt
+}
+ 
+
+void transmitSync(){   
+    transmitByte(0xFE); // sync
+    transmitByte(0x19); // sync
+}
+
+/*
+void transmitCommonMotor(void){
+
+    //sink has already been called
+    transmitByte(0x01);
+    transmitByte(0x06);
+    transmitByte(0x04);
+    transmitByte(0x00);
+}
+
+
+void testPulseMotor(int motorA_dir_IN, int motorA_speed_IN, int motorB_dir_IN, int motorB_speed_IN){
+    
+    transmitSync();
+    transmitCommonMotor();
+    
+    transmitByte(motorA_dir_IN);
+    transmitByte(motorA_speed_IN);   
+    transmitByte(motorB_dir_IN);  
+    transmitByte(motorB_speed_IN);
+}
+*/
+ 
+
+void getPCLS(){
+
+    //recive register has a buffer of 2 bytes
+    //ask pcls to get data from controller
+    
+    transmitSync();
+    
+    transmitByte(0x01); // MSG ID
+    transmitByte(0x04); // MSG ID
+    transmitByte(0x00);
+    transmitByte(0x00);
+}
+
+void getUserData(){
+    //ask pcls to get data from the RF controller
+    
+    transmitSync();
+    
+    transmitByte(0x01); // MSG ID
+    transmitByte(0x05); // MSG ID
+    transmitByte(0x00);
+    transmitByte(0x00);
+}
+
+void main(void) {
+    
+    setUp();
+    
+    //backward on motor A test
+    //02 64 00 00
+    //0x64 is max speed
+    
+    while(1){
+        waitForIt(); //wait for Shift REG
+        ///////////////bD    aS    aD     bS
+        //testPulseMotor(0x01, 0x64, 0x01, 0x64);
+        getPCLS();  
+        __delay_ms(10);
+        //getUserData();
+        //__delay_ms(10);
+        sort_data();
+        __delay_ms(250);
+        
+    } 
+
+    return;
+}
+
