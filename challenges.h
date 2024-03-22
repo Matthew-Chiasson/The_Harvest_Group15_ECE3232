18190e22dfb1cce5b4ddce64124ae35514c8b298
EthanDever <159584445+EthanDever@users.noreply.github.com>
2024-03-22 17:47:51 -0300

    Create challenges.h

---
+++ challenges.h
@@ -0,0 +1,16 @@
+
+#ifndef CHALLENGES_H
+#define CHALLENGES_H
+
+//#include <xc.h>
+#include "data_sorting.h"  // For switchBMSB and switchDMSB
+
+// prototypes
+void challenges();
+void getPCLS(); 
+void transmitSync(); 
+void transmitByte(int byte);  
+
+#endif /* CHALLENGES_H */
+
+
