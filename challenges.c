4d5fbe2666f580e589738c3cbc837573d83a4126
EthanDever <159584445+EthanDever@users.noreply.github.com>
2024-04-03 16:23:49 -0300

    Update challenges.c

--- challenges.c
+++ challenges.c
@@ -20,57 +20,112 @@ void challenges(){
             ANSELBbits.ANSB1 = 0; //RB1 is digital input
             
             // Let RC4, RC3, and RB5 be LED0, LED1, and LED2 respectively
-            TRISCbits.TRISC4 = 0; // LED output pins
-            TRISCbits.TRISC3 = 0; 
-            TRISBbits.TRISB5 = 0; 
+         //   TRISCbits.TRISC4 = 0; // LED output pins
+         //   TRISCbits.TRISC3 = 0; 
+          //  TRISBbits.TRISB5 = 0; 
             
     
     if      (potentiometerA>=1000 && potentiometerA<1166)
     { 
-        LATCbits.LATC4 = 1;
-        LATCbits.LATC3 = 0;
-        LATBbits.LATB5 = 0;
+      //  LATCbits.LATC4 = 1;
+     //   LATCbits.LATC3 = 0;
+     //   LATBbits.LATB5 = 0;
+
+            if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
+    
     }
     else if (potentiometerA>=1166 && potentiometerA<1333)
     {
-        LATCbits.LATC4 = 0;
-        LATCbits.LATC3 = 1;
-        LATBbits.LATB5 = 0;
+   //     LATCbits.LATC4 = 0;
+     //   LATCbits.LATC3 = 1;
+      //  LATBbits.LATB5 = 0;
+
+            if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
     }
     else if (potentiometerA>=1333 && potentiometerA<1500)
     {
-        LATCbits.LATC4 = 1;
-        LATCbits.LATC3 = 1;
-        LATBbits.LATB5 = 0;
+     //   LATCbits.LATC4 = 1;
+     //   LATCbits.LATC3 = 1;
+      //  LATBbits.LATB5 = 0;
+
+            if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
     }
     else if (potentiometerA>=1500 && potentiometerA<1667)
     {
-        LATCbits.LATC4 = 0;
-        LATCbits.LATC3 = 0;
-        LATBbits.LATB5 = 1;
+    //    LATCbits.LATC4 = 0;
+    //    LATCbits.LATC3 = 0;
+    //    LATBbits.LATB5 = 1;
+
+            if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
     }
     else if (potentiometerA>=1667 && potentiometerA<1834)
     {
-        LATCbits.LATC4 = 1;
-        LATCbits.LATC3 = 0;
-        LATBbits.LATB5 = 1;
+     //   LATCbits.LATC4 = 1;
+     //   LATCbits.LATC3 = 0;
+     //   LATBbits.LATB5 = 1;
+
+            if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
     }
     else if (potentiometerA<=1834 && potentiometerA<2000)
     {
-        LATCbits.LATC4 = 0;
-        LATCbits.LATC3 = 1;
-        LATBbits.LATB5 = 1;
+     //   LATCbits.LATC4 = 0;
+     //   LATCbits.LATC3 = 1;
+     //   LATBbits.LATB5 = 1;
+
+            if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
     }
          
-//    if (PORTBbits.RB1 == 0) //While IR sensor is active
-//        {
-//            LATCbits.LATC2 = 1; //turn LED on
-//        }
-//    else
-//        {
-//            LATCbits.LATC2 = 0; // necessary?
-//        }
-    }
+    if (PORTBbits.RB1 == 0) //While IR sensor is active
+        {
+            LATCbits.LATC2 = 1; //turn LED on
+        }
+    else
+       {
+            LATCbits.LATC2 = 0; // necessary?
+        }
+    
 
 // LASER TURRET DEFENSE CHALLENGE
 // SWD to send shield code
